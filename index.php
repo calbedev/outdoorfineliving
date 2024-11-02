e10593f3f1822ecd0e21215e37f8c8600d4fe058
Calbe <calbedev@gmail.com>
2024-11-02 08:42:44 +0200

    w

--- index.php
+++ index.php
@@ -2,27 +2,7 @@
     <!-- Main Content Wrapper Start -->
         <main class="main-content-wrapper">
                 <!-- Slider area Start -->
-                <?php include 'components/slider.php' ?>
-                <!-- Slider area End -->
                 
-                <!-- Top Sale Area Start -->
-                <?php include 'components/destaque.php' ?>
-                <!-- Top Sale Area End -->
-
-                <!-- Feature Product Area Start -->
-                <?php include 'components/anuncio.php' ?>
-                <!-- Feature Product Area End -->
-
-                <!-- Product Tab Area Start -->
-                <?php include 'components/moveis.php' ?>
-                <!-- Product Tab Area End -->
-
-                <!-- Brand Logo Area Start -->
-                
-                <!-- Brand Logo Area End -->
-
-                <!-- Best Sale Product Area Start -->
-                <?php include 'components/sobre.php' ?>
                 <!-- Best Sale Product Area End -->
 
                 <!-- Blog Area Start -->
