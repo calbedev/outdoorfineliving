89cf0093e5271d1f0b759581dbd245815cd4f8cc
Calbe <calbedev@gmail.com>
2024-11-02 08:43:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
         <main class="main-content-wrapper">
                 <!-- Slider area Start -->
                 
+                <!-- Product Tab Area End -->
+
+                <!-- Brand Logo Area Start -->
+                
+                <!-- Brand Logo Area End -->
+
+                <!-- Best Sale Product Area Start -->
+                <?php include 'components/sobre.php' ?>
                 <!-- Best Sale Product Area End -->
 
                 <!-- Blog Area Start -->
